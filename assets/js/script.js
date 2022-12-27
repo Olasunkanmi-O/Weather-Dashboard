8adf84f7b4500e3e7d7948f7f7b73e8c193ec1ac
Olasunkanmi <olasunkanmi.m.owolabi@gmail.com>
2022-12-27 13:11:55 +0000

    inserted all HTML elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,81 @@
 
 
+// https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric
+// https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric
+
+
 
 var apiKey = '6df1a64a30a7da7bf9c5be5027c23bd8';
-var city = 'toronto' 
-var today = $('#today')
-var forecast = $('#forecast')
+var city = 'London'
+var baseUrl = 'https://api.openweathermap.org/data/2.5/';
+var currentWeather = baseUrl + `weather?&appid=${apiKey}&units=metric&`;
+var forecastWeather = baseUrl + `forecast?&appid=${apiKey}&units=metric&`
 
 
 
 
-// code to get the data of current weather condition from the Open Weather URl
-$.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-    .then(function (currentData) {
-        console.log(currentData)
+function getInput(city) {
+    $.get(currentWeather + `q=${city}`)
+        .then(function (currentData) {
 
-        var lon = currentData.coord.lon;
-        var lat = currentData.coord.lat;
-        var date = moment().format('L');
-        var icon = currentData.weather[0].icon
+            console.log(currentData)
 
-        today.append(`
-        <div class="currentSummary">
-            <h2>${currentData.name}<small class="todayDate">(${date})</small>
-            <img src="https://openweathermap.org/img/w/${icon}.png" alt="weather_icon">
-            </h2>
-            <p>Temperature: ${Math.round(currentData.main.temp)}&degC</p>
-            <p>Humidity: ${currentData.main.humidity}%</p>
-            <p>Wind Speed: ${currentData.wind.speed}m/s</p>
-            <p class="mb">UV index: ${currentData.wind.speed}</p>
-        </div>
-        `)
+            var lon = currentData.coord.lon;
+            var lat = currentData.coord.lat;
+            var date = moment().format('L');
+            var icon = currentData.weather[0].icon
 
 
-        // code to get the data of future weather condition from the Open Weather URl
-        $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
-            .then(function (forecastData) {
-                console.log(forecastData.list)
-                for (var obj of forecastData.list){
-                    var imageIcon = obj.weather[0].icon
-                   if(`${obj.dt_txt}`.includes('12:00:00')){
-                    console.log(`
-                    https://openweathermap.org/img/w/${imageIcon}.png}
-                    ${obj.dt_txt}
-                    ${obj.main.temp}s
-                    ${obj.main.humidity}
-                    `)
-                }}
-                
+            $.get(forecastWeather + `lat=${lat}&lon=${lon}`)
+                .then(function (forecastData) {
+
+                    var today = $('#today')
+                    var forecast = $('#forecast')
+
+                    today.append(`
+                    <div>
+                        <h2>${currentData.name}<small class="todayDate">(${date})</small>
+                            <img src="https://openweathermap.org/img/w/${icon}.png" alt="weather_icon">
+                        </h2>
+                        <p>Temperature: ${Math.round(currentData.main.temp)}&degC</p>
+                        <p>Humidity: ${currentData.main.humidity}%</p>
+                        <p>Wind Speed: ${currentData.wind.speed}m/s</p>
+                        <p class="mb">UV index: ${currentData.wind.speed}</p>                        
+                    </div>
+                    
+                    
+                `)
 
 
-            });
+                    for (var obj of forecastData.list) {
+                        var imageIcon = obj.weather[0].icon
+                        if (`${obj.dt_txt}`.includes('12:00:00')) {
+                            forecast.append(`
+                            
+                            <div>
+                                <h5>${moment(obj.dt_txt).format('L')}</h5>
+                                <img src="https://openweathermap.org/img/w/${imageIcon}.png" alt="weather_icon">                    
+                                <p>Temp: ${obj.main.temp}&degC</p>
+                                <p>Humidity: ${obj.main.humidity}%</p>
+                            </div>
+                            `)
+                        }
+                    }
+
+                })
+
+        }
+        )
+
+}
+getInput(city)
 
 
-    });
+function init(){
+    $('#search-button').on('click', getInput);
+}
+
+init()
 
 
 
-
-
-
-//      
